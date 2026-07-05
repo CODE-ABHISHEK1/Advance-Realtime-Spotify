0633965ac948ceae55b1ae4d207975ae749674ff
Abhi <a.s.jain7475@gmail.com>
2026-07-05 12:06:33 +0530

    Fix Express production route because of express 5

--- backend/src/index.js
+++ backend/src/index.js
@@ -70,8 +70,11 @@ app.use("/api/stats", statsRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+
+  app.use((req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../frontend/dist/index.html")
+    );
   });
 }
 
